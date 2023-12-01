f1272daa884d37ec8d2adf1ce4fcc256d71973dd
ABC01LP015\enes.sahan <enes.sahan@abclojistik.com>
2023-12-01 14:38:13 +0300

    Add project files.

---
+++ RunApp/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using RunGroopWebApp.Models;
+
+namespace RunApp.Models
+{
+    public class AppUser
+    {
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Races { get; set; }
+    }
+}
---
+++ RunApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RunApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
