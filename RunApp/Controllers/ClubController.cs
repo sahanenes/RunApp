511c09404437d6866414cc386159a05183790e57
ABC01LP015\enes.sahan <enes.sahan@abclojistik.com>
2023-12-02 12:12:21 +0300

    views

---
+++ RunApp/Controllers/ClubController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunApp.Data;
+using RunApp.Models;
+
+namespace RunApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+           _context = context;
+        }
+        public IActionResult Index()
+        {
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+    }
+}
---
+++ RunApp/Controllers/RaceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunApp.Data;
+
+namespace RunApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var races = _context.Races.ToList();
+            return View(races);
+        }
+    }
+}
