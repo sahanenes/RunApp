7b271565c4c96bcde2216f55af33b3b8a1b18539
ABC01LP015\enes.sahan <enes.sahan@abclojistik.com>
2023-12-07 11:44:30 +0300

    upload photo

--- RunApp/Program.cs
+++ RunApp/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using RunApp.Data;
+using RunApp.Helpers;
 using RunApp.Interfaces;
 using RunApp.Repository;
+using RunApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IClubRepository, ClubRepository>();
 builder.Services.AddScoped<IRaceRepository, RaceRepository>();
-
+builder.Services.AddScoped<IPhotoService, PhotoService>();
+builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
