ebc9e5648985ad700787cc046ae04f78e8579228
sahanenes <sahanenesss@gmail.com>
2024-01-06 15:43:42 +0700

    list/edit users

---
+++ RunApp/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RunApp.Interfaces;
+using RunApp.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RunApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        [HttpGet("users")]
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userRepository.GetAllUsers();
+            List<UserViewModel> result = new List<UserViewModel>();
+            foreach (var user in users)
+            {
+                var userViewModel = new UserViewModel()
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Pace = user.Pace,
+                    Mileage = user.Mileage,
+                };
+                result.Add(userViewModel);
+            }
+            return View(result);
+        }
+
+        public async Task<IActionResult> Detail(string id)
+        {
+            var user = await _userRepository.GetUserById(id);
+            var userDetailViewModel = new UserDetailViewModel()
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Pace = user.Pace,
+                Mileage = user.Mileage,
+            };
+            return View(userDetailViewModel);
+        }
+    }
+}
+
---
+++ RunApp/ViewModels/UserDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace RunApp.ViewModels
+{
+	public class UserDetailViewModel
+	{
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+    }
+}
+
