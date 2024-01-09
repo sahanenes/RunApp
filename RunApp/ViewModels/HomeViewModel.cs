3a259d48480ac875825c203ef3843b500884aa0a
sahanenes <sahanenesss@gmail.com>
2024-01-09 10:41:27 +0700

    ipinfo

---
+++ RunApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using RunApp.Models;
+
+namespace RunApp.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Club> Clubs { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
+
